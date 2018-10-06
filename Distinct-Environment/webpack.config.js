ea1b2f6af6c2d0c2a3d9461502859a7adc9d61b5
littleLane <littleLaneBye@gmail.com>
2018-10-06 13:06:15 +0800

    添加 环境区分 case

---
+++ Distinct-Environment/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+  entry: './src',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        
+        // 注意这里 loader 配置的顺序，样式文件依次从右到左经过 use 配置 loader 的处理
+        // css-loader 是解析样式文件
+        // style-loader 是将解析后的样式渲染在页面上
+        use: ExtractTextWebpackPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader']
+        }),
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin('index.css'),
+  ],
+  devServer: {},
+  mode: process.env.NODE_ENV,
+}
